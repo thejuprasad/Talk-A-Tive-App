255ca6c0a5f3c13c5f9c89be88f043d01feae572
Theju Prasad <tthejuprasad00@gmail.com>
2023-10-10 14:23:08 +0530

    Almost backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ const dotenv=require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors=require("colors");
+const userRoutes=require('./routes/userRoutes');
 
 
 const app=express();
 dotenv.config();
 connectDB();
+
+app.use(express.json());
+
 
 
 
@@ -16,15 +20,17 @@ app.get('/',(req,res)=>{
     res.send("API is running successfully");
 })
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats);
-});
+// app.get("/api/chat",(req,res)=>{
+//     res.send(chats);
+// });
 
-app.get("/api/chat/:id",(req,res)=>{
-    const singleChat=chats.find((c)=>c._id===req.params.id);
-    res.send(singleChat);
+// app.get("/api/chat/:id",(req,res)=>{
+//     const singleChat=chats.find((c)=>c._id===req.params.id);
+//     res.send(singleChat);
 
-});
+// });
+
+app.use('/api/user',userRoutes)
 
 const PORT=process.env.PORT || 5000; 
 app.listen(5000,console.log(`server is running at PORT ${PORT}`.yellow.bold));
